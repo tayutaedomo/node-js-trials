1ea419b339158d2d565cad11b38e7df2de81a02d
tayutaedomo <tayutaedomo@gmail.com>
2016-08-04 20:59:42 +0900

    Add bluebird trial

---
+++ scripts/bluebird_why_promises.js
@@ -0,0 +1,41 @@
+'use strict';
+
+//
+// Refer: http://bluebirdjs.com/docs/why-promises.html
+//
+
+var Promise = require("bluebird");
+var fs = require("fs");
+Promise.promisifyAll(fs);
+
+var file_path = __dirname + '/../package.json';
+console.log(file_path);
+
+fs.readFile(file_path, function (err, val) {
+  if (err) {
+    console.error('unable to read file');
+  }
+  else {
+    try {
+      val = JSON.parse(val);
+      console.log(val);
+    }
+    catch (e) {
+      console.error('invalid json in file');
+    }
+  }
+});
+
+
+fs.readFileAsync(file_path)
+  .then(JSON.parse)
+  .then(function (val) {
+    console.log(val);
+  })
+  .catch(SyntaxError, function (e) {
+    console.error("invalid json in file");
+  })
+  .catch(function (e) {
+    console.error("unable to read file");
+  });
+
