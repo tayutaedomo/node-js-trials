5d2cd59a2f279871cb40a0a18b3b2feb4c54ab6a
tayutaedomo <tayutaedomo@gmail.com>
2016-10-01 18:12:19 +0900

    Fix request_promise_fs_write logic

--- scripts/request_promise_fs_write.js
+++ scripts/request_promise_fs_write.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
+var path = require('path');
 var rp = require('request-promise');
+var Promise = require('bluebird');
+var fs = require('fs');
+Promise.promisifyAll(fs);
 
 var url = 'https://s3.amazonaws.com/node-js-sdk-trial.tayutaedomo.net/white2.png';
 
-rp(url)
-  .then(function (content) {
-    console.log(content);
-  })
-  .catch(function (err) {
-    console.error(err);
-  });
+rp(url).then(function (content) {
+  console.log(content);
+
+  var file_path = path.join(__dirname, '../tmp/request_promise_fs_write_white2.png');
+  return fs.writeFileAsync(file_path, content, 'binary'); // Created file is broken
+
+// }).then(function (result) {
+//   console.log(result);
+
+}).catch(function (err) {
+  console.error(err);
+});
 
