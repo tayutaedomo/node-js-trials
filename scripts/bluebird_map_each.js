38cf0773f0f49834843924ca13a14694e799939c
tayutaedomo <tayutaedomo@gmail.com>
2017-09-28 20:04:43 +0900

    Try bluebird.map with iteratable object

--- scripts/bluebird_map_each.js
+++ scripts/bluebird_map_each.js
@@ -12,6 +12,14 @@ Promise.map([1, 2, 3], function(a) {
   console.log('Promise.map result:', result);
 });
 
+// with Map
+var map = new Map([[1,11], [2,22]]);
+Promise.map(map, function(a) {
+  console.log('In map with Map,', a);
+  return a;
+}).then(function(result) {
+  console.log('Promise.map result:', result);
+});
 
 //
 // Refer: http://bluebirdjs.com/docs/api/promise.each.html
