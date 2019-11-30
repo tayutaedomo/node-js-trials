e35b8417502bd9dc9d4701d9e5dc1d82c6093c3d
tayutaedomo <tayutaedomo@gmail.com>
2019-11-30 18:31:08 +0900

    Fix zip/main.js

--- scripts/zip/main.js
+++ scripts/zip/main.js
@@ -4,7 +4,38 @@ const fs = require('fs');
 const JSZip = require('jszip');
 
 
-fs.readFile(path.join(__dirname, 'archives', 'file.zip'), (err, data) => {
+// fs.readFile(path.join(__dirname, 'archives', 'file.zip'), (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//
+//   JSZip.loadAsync(data).then(function (zip) {
+//     console.log(zip);
+//
+//   }).catch(err => {
+//     console.error(err);
+//   });
+// });
+/*
+{ files:
+   { 'a.txt':
+      { name: 'a.txt',
+        dir: false,
+        date: 2019-11-30T18:09:36.000Z,
+        comment: null,
+        unixPermissions: 33188,
+        dosPermissions: null,
+        _data: [Object],
+        _dataBinary: true,
+        options: [Object] } },
+  comment: null,
+  root: '',
+  clone: [Function] }
+ */
+
+
+fs.readFile(path.join(__dirname, 'archives', 'directory.zip'), (err, data) => {
   if (err) {
     console.error(err);
     return;
@@ -17,4 +48,30 @@ fs.readFile(path.join(__dirname, 'archives', 'file.zip'), (err, data) => {
     console.error(err);
   });
 });
+/*
+{ files:
+   { 'directory/':
+      { name: 'directory/',
+        dir: true,
+        date: 2019-11-30T18:23:50.000Z,
+        comment: null,
+        unixPermissions: 16877,
+        dosPermissions: null,
+        _data: [Promise],
+        _dataBinary: true,
+        options: [Object] },
+     'directory/a.txt':
+      { name: 'directory/a.txt',
+        dir: false,
+        date: 2019-11-30T18:09:36.000Z,
+        comment: null,
+        unixPermissions: 33188,
+        dosPermissions: null,
+        _data: [Object],
+        _dataBinary: true,
+        options: [Object] } },
+  comment: null,
+  root: '',
+  clone: [Function] }
+ */
 
