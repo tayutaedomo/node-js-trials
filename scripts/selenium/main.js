e5b29cde1f4fdea83fd6f7240a23c89b611c0021
tayutaedomo <tayutaedomo@gmail.com>
2019-12-21 14:54:18 +0900

    Add get handling codes

--- scripts/selenium/main.js
+++ scripts/selenium/main.js
@@ -1,26 +1,49 @@
 
 const path = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
 const webdriver = require('selenium-webdriver');
 const { Builder, By, until } = webdriver;
 const { ServiceBuilder } = require('selenium-webdriver/chrome');
 
-const capabilities = webdriver.Capabilities.chrome();
-capabilities.set('chromeOptions', {
-  args: [
-    '--headless',
-    '--no-sandbox',
-    '--disable-gpu',
-    `--window-size=1980,1200`
-    // other chrome options
-  ]
-});
 
 const main = async () => {
+  const driver = await create_driver();
+
+  // Refer: https://qiita.com/tonio0720/items/70c13ad304154d95e4bc
+  await driver.get('https://www.youtube.com/');
+
+  await driver.wait(until.elementLocated(By.id('search')), 10000);
+
+  let base64 = await driver.takeScreenshot();
+  let buffer = Buffer.from(base64, 'base64');
+
+  await promisify(fs.writeFile)('screenshot.jpg', buffer);
+
+  await driver.quit();
+};
+
+
+const create_driver = async () => {
+  const capabilities = webdriver.Capabilities.chrome();
+  capabilities.set('chromeOptions', {
+    args: [
+      '--headless',
+      '--no-sandbox',
+      '--disable-gpu',
+      `--window-size=1980,1200`
+      // other chrome options
+    ]
+  });
+
   const chromedriver_path = path.join(__dirname, '..', '..', 'bin', 'chromedriver');
   const service_builder = new ServiceBuilder(chromedriver_path);
-  const driver = await new Builder().withCapabilities(capabilities).setChromeService(service_builder).build();
-  driver.quit();
+  const builder = new Builder().withCapabilities(capabilities).setChromeService(service_builder);
+  const driver = await builder.build();
+
+  return driver;
 };
+
 
 
 main().then(() => {
